7c131d6396ee81837f4a2977b6c163a61da6c08f
Rémy Hannequin <remy@thoughtbot.com>
2025-08-08 17:02:03 +0200

    Build design and start adding real data

--- app/controllers/caelus/home_controller.rb
+++ app/controllers/caelus/home_controller.rb
@@ -12,6 +12,10 @@ module Caelus
         Uranus.new,
         Neptune.new
       ]
+      @observer = Astronoby::Observer.new(
+        latitude: Astronoby::Angle.from_degrees(51.5074),
+        longitude: Astronoby::Angle.from_degrees(-0.1278)
+      )
     end
   end
 end
