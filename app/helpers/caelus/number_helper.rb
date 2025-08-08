3c8e57c619011f4ebe5dc4da6b2a8553db92060b
Rémy Hannequin <remy@thoughtbot.com>
2025-08-08 17:02:03 +0200

    Build design and start adding real data

--- app/helpers/caelus/number_helper.rb
+++ app/helpers/caelus/number_helper.rb
@@ -4,7 +4,12 @@ module Caelus
   module NumberHelper
     def format_number(number, precision: 2, unit: nil)
       formatted_number = format("%.#{precision}f", number)
-      formatted_number += " #{unit}" if unit
+      if unit
+        if [:au, :kmps].include?(unit)
+          unit_str_key = {au: "astronomical_unit", kmps: "kilometers_per_second"}[unit]
+          formatted_number += " #{I18n.t("caelus.units.#{unit_str_key}.symbol")}"
+        end
+      end
       formatted_number
     end
   end
