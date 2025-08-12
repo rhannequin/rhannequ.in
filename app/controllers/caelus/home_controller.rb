0de0ab99dc15f8b93f6850c19ca6dd9bc1b9cfc4
Rémy Hannequin <remy@thoughtbot.com>
2025-08-12 23:32:31 +0200

    Add a bit more data

--- app/controllers/caelus/home_controller.rb
+++ app/controllers/caelus/home_controller.rb
@@ -16,6 +16,33 @@ module Caelus
         latitude: Astronoby::Angle.from_degrees(51.5074),
         longitude: Astronoby::Angle.from_degrees(-0.1278)
       )
+      @twilight_events = Astronoby::TwilightCalculator.new(
+        observer: @observer,
+        ephem: SPK.inpop19a
+      ).event_on(Date.today)
+      @next_twilight_events = Astronoby::TwilightCalculator.new(
+        observer: @observer,
+        ephem: SPK.inpop19a
+      ).event_on(Date.tomorrow)
+      @moon = Astronoby::Moon.new(
+        instant: Astronoby::Instant.from_time(Time.now),
+        ephem: SPK.inpop19a
+      )
+      moon_rts = Astronoby::RiseTransitSetCalculator.new(
+        body: Astronoby::Moon,
+        observer: @observer,
+        ephem: SPK.inpop19a
+      ).event_on(Date.today)
+      @moon_rising_time = moon_rts.rising_time
+      @moon_setting_time = if moon_rts.setting_time > @moon_rising_time
+        moon_rts.setting_time
+      else
+        Astronoby::RiseTransitSetCalculator.new(
+          body: Astronoby::Moon,
+          observer: @observer,
+          ephem: SPK.inpop19a
+        ).event_on(Date.tomorrow).setting_time
+      end
     end
   end
 end
