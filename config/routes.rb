40d9b2dda0f007fad53153000defef7435a2d310
Rémy Hannequin <remy.hannequin@gmail.com>
2025-08-31 22:11:00 +0200

    [caelus] Support caelus subdomain (#22)
    I want Caelus to be accessible with `caelus` subdomain in HTTPS.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
-  root "about#index"
+  constraints(Constraints::SubdomainConstraint.new(subdomain: "caelus")) do
+    scope module: :caelus, as: :caelus do
+      root "home#index"
 
-  namespace :caelus do
-    root "home#index"
-
-    resource :location, only: [:edit, :update], controller: :location
-    resources :planets, only: [:show], param: :id
-    resource :privacy_policy, only: :show, controller: :privacy_policy
-    resource :cookie_consent,
-      only: [:create, :destroy],
-      controller: :cookie_consent
+      resource :location, only: [:edit, :update], controller: :location
+      resources :planets, only: [:show], param: :id
+      resource :privacy_policy, only: :show, controller: :privacy_policy
+      resource :cookie_consent,
+        only: [:create, :destroy],
+        controller: :cookie_consent
+    end
   end
+
+  root "about#index"
 end
